9139d5c463e5bce9731f62029dd788dbb3986694
Joao Victor Saraiva <joaovsaraivasousa@gmail.com>
2022-12-20 10:23:48 -0300

    first usage of the json gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'discordrb'
+gem 'json'
 
 group :development, :test do 
   gem 'byebug'
--- lib/punch_the_clock.rb
+++ lib/punch_the_clock.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 # This class represent an absctracion on the action of punch the clock.
 class PunchTheClock
   def perfom_start_time(start_time, employer)
@@ -8,5 +10,11 @@ class PunchTheClock
 
   def perfom_end_time(end_time, employer)
     employer.end_time = end_time
+  end
+
+  def save_day_perfom(employer)
+    File.open(file_path, "w") do |file|
+      file.write(hashed_object.to_json)
+    end
   end
 end
